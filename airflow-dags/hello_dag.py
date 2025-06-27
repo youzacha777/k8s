3804d182f0beec0eeb01f2baca9991e01bda7cbb
youzacha777 <youzacha777@gmail.com>
2025-06-27 13:03:00 +0900

    Update hello_dag.py

--- airflow-dags/hello_dag.py
+++ airflow-dags/hello_dag.py
@@ -12,17 +12,17 @@ with DAG(
     
     start = BashOperator(
         task_id="start_task",
-        bash_command="echo 'Starting Dag execution.."
+        bash_command="echo 'Starting Dag execution..'"
     )
 
     middle = BashOperator(
         task_id="middle_task",
-        bash_command="echo 'Middling Dag execution.."
+        bash_command="echo 'Middling Dag execution..'"
     )
 
     end = BashOperator(
         task_id="end_task",
-        bash_command="echo 'Ending Dag execution.."
+        bash_command="echo 'Ending Dag execution..'"
     )
 
     start >> middle >> end
